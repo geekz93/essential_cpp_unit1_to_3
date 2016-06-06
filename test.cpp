15bb87f89c7c17349fbae1e07f421808cabfa929
zooo <imzooo@163.com>
2016-06-06 10:14:57 +0800

    通用函数-fliter

--- test.cpp
+++ test.cpp
@@ -1,28 +1,15 @@
 #include<iostream>
 #include<vector>
+#include<list>
 #include<functional>
-#include<algorithm>
 using namespace std;
-void disp(char *comments, vector<int> &ivec)
+template <typename InputIterator>
+void disp(char *comments, InputIterator first, InputIterator last)
 {
 	cout << comments; 
-	vector<int>::iterator iter = ivec.begin();
-	for(; iter!= ivec.end(); ++iter)
-		cout << *iter << ' ';
+	for(; first!= last; ++first)
+		cout << *first << ' ';
 	cout << endl; 
-}
-
-vector<int> filter1(const vector<int> &vec, int val, less<int> &lt)
-{
-	vector<int> nvec;
-	vector<int>::const_iterator iter = vec.begin();
-	while( (iter = find_if(iter, vec.end(),
-		bind2nd(lt, val))) != vec.end() )
-	{
-		nvec.push_back( *iter );
-		++iter;
-	}
-	return nvec;
 }
 
 //generic
@@ -30,23 +17,25 @@ template<typename InputIterator, typename OutputIterator,
 		 typename ElemType, typename Comp>
 OutputIterator
 filter( InputIterator first, InputIterator last, 
-		OutputIterator at, const ElemType &val, Comp pred)
+		OutputIterator &at, const ElemType &val, Comp pred)
 {
 	while(( first = find_if(first, last, 
 			bind2nd(pred, val))) != last )
 	{
-		cout << "found value: " << first << endl;
-		*at++ = *first;
+		cout << "found value: " << *first << endl;
+		at.push_back(*first++);
 	}
 	return at;
 }
-
 
 int main()
 {
 	int ia[]={1, 34, 2, 45, 3, 1 ,53};
 	vector<int> iv1(ia, ia+7);
-	vector<int> iv2(7, 2); 
-	
+	list<int> ivRes;
+	int num = 6;
+	filter( iv1.begin(), iv1.end(), 
+			ivRes, num, greater<int>() );//greater<type> -> ´óÓÚnum 
+	disp("ilr: ", ivRes.begin(), ivRes.end());
 	return 0;
 }
