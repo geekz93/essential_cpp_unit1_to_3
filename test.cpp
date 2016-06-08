539d1af05e3d6f01695bb79c0d16333b1a0cc329
zooo <imzooo@163.com>
2016-06-08 14:38:10 +0800

    exercise3.1

--- test.cpp
+++ test.cpp
@@ -1,30 +1,52 @@
 #include<iostream>
 #include<vector>
-#include<string>
 #include<iterator>
 #include<algorithm>
-#include<fstream> 
+#include<string>
+#include<fstream>
+#include<set>
+#include<map>
 using namespace std;
-//iostream iterators
+//exercise3.1
+//read a text file, store each word as a key in a map
+//the key value is the count of each word
+//define a set containing words such as "a, an, or, 
+//the, and, but"
+
 int main()
 {
-	ifstream ifile("inputFile.txt");
-	ofstream ofile("sortFile.txt");
-	if( !ifile || !ofile )
+	ifstream ifile("article.txt");
+	if(!ifile)
 	{
-		cerr<< "open file err... " << endl;
-		return -1; 
+		cerr<< "open file err..." << endl;
+		return -1;
 	}
-		
-	istream_iterator<string> is(ifile);
-	istream_iterator<string> endfile;
 	
-	vector<string> str;
-	copy(is, endfile, back_inserter(str) );
+	istream_iterator<string> istr(ifile);
+	istream_iterator<string> eof;
 	
-	sort(str.begin(), str.end());
+	//vector<string> ReadStr;
+	string sExWord[]={"a", "an", "or", "the", "and", "but"};
+	set<string> ssExWord(sExWord, sExWord+6);
+	set<string>::iterator sit = ssExWord.begin();
+/*//display set
+	for(; sit!=ssExWord.end(); ++sit)
+		cout << *sit << endl;
+*/
+	map<string, int> WordAcount;
 	
-	ostream_iterator<string> os(ofile, " ");
-	copy(str.begin(), str.end(), os);
+	for( ; istr!=eof; ++istr)
+	{
+		if( ssExWord.count(*istr) )
+			continue;
+		WordAcount[*istr]++;
+	}
+	map<string, int>::iterator miter=WordAcount.begin();
+for( ; miter!=WordAcount.end(); ++miter)
+	{
+		cout<< miter->first << " "
+			<< miter->second << endl;
+	}
+	
 	return 0;
 }
