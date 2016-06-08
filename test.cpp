d35370aa00cf8d34241edae87eda7e7c54af527b
zooo <imzooo@163.com>
2016-06-08 10:46:38 +0800

    iostream iterator to file

--- test.cpp
+++ test.cpp
@@ -1,27 +1,30 @@
 #include<iostream>
 #include<vector>
+#include<string>
 #include<iterator>
 #include<algorithm>
-#include<string>
+#include<fstream> 
 using namespace std;
 //iostream iterators
 int main()
 {
-	istream_iterator<string> inputs(cin);//定义输入向量：与标准数入流绑定 
-	istream_iterator<string> endfile;//输入的结束 end_of_file 
+	ifstream ifile("inputFile.txt");
+	ofstream ofile("sortFile.txt");
+	if( !ifile || !ofile )
+	{
+		cerr<< "open file err... " << endl;
+		return -1; 
+	}
+		
+	istream_iterator<string> is(ifile);
+	istream_iterator<string> endfile;
 	
-	vector<string> text;
-	copy(inputs, endfile, back_inserter(text));//将输入的数据存入text向量由于不知道输入数据的大小故使用back_inserter 
+	vector<string> str;
+	copy(is, endfile, back_inserter(str) );
 	
-	sort(text.begin(), text.end());//排序 
+	sort(str.begin(), str.end());
 	
-	ostream_iterator<string> outputs(cout, " ");//定义输出向量：与标准输出流绑定 
-	copy(text.begin(), text.end(), outputs);//输出 
-	//同样可以输出数据 
-	/* 
-	vector<string>::iterator it = text.begin();
-	while( it!=text.end())
-	*outputs = *it++;//*outputs++没有区别？ 
-	*/
+	ostream_iterator<string> os(ofile, " ");
+	copy(str.begin(), str.end(), os);
 	return 0;
 }
